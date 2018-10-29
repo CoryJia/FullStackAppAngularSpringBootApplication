c7e165b0d55b97b5ba4f3b75aaec5cb4004ef6fb
jianbojia <coryjia@gmail.com>
2018-10-29 08:44:48 -0700

    Oct/29/2018 V1

---
+++ src/main/java/com/keepwalking/FullStackAppAngularSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.keepwalking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class FullStackAppAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackAppAngularSpringBootApplication.class, args);
+	}
+
+}
